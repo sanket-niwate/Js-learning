45503b2682059e2929aa297b286f887983bf33b4
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-06-28 11:09:18 +0000

    variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accoutNo =454544;
+let accountEmail ="sanekt@gmail.com";
+var accountPassword="446564"
+accountCity ="jaipur"
+let accountState;
+
+// accoutNo =5 //not allowed
+
+accountEmail="sanket1@gmail.com"
+accountPassword="675466"
+accountCity="mumbai"
+console.log(accoutNo);
+
+/*prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
