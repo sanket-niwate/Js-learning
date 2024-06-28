a12e7c8c92bc46254e2895c958829b832348402d
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-06-28 11:36:34 +0000

    datatypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all js code as newer version
+
+//alert (3+3) // we are using nodejs nor browser
+
+
+console.log(3+
+    
+    3)//code readability should be high
+
+    console.log("sanket")
+
+    let name="sanket"
+    let age=32
+    let isLogged =false
+
+//primitive datatypes
+    // number => 2 to power 53
+    // bigint
+    // string=> ""
+    // boolean=> true/false
+    // null => standalone CSSMathValue   //null is an object
+    // undefined =>
+    // symvbol=>unique
+
+
+
+
+    // Object
+
+    console.log(typeof null);//object
+    console.log(typeof undefined);//object
