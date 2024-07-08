0d24af9396717c48dd6586ed97ec6d042b94140d
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-07-08 11:11:21 +0000

    objects is done

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,41 @@
+//singleton
+//object.create
+
+//object literals
+
+const mySym=  Symbol("key1")
+
+const JsUser={
+    name:"sanket",
+    age:18,
+    [mySym]:"mykey1",
+    "full name":"sanket niwate",
+    location:"maharashtra",
+    email:"sanket@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// console.log(JsUser);
+
+
+JsUser.email = "sanket@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "sanket@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
