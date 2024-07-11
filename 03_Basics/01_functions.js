ebf787cf799a36e842c4beab8c3ba4306f36587e
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-07-11 10:36:53 +0000

    functions done

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -49,3 +49,34 @@ function loginUserMessage(username = "sk"){
 // console.log(loginUserMessage(""))
 // console.log(loginUserMessage("sanket"))
 
+function calculateCartPrice(...num1) {
+    return num1
+
+}
+
+// console.log(calculateCartPrice(5,1,2,5));
+
+const user={
+    username:"sanket",
+    prices:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
