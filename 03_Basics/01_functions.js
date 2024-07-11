9a48466283ea2cb49314f4c506de9789aec45d76
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-07-11 10:09:25 +0000

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,51 @@
+// console.log("s");
+// console.log("a");
+// console.log("n");
+// console.log("k");
+// console.log("e");
+// console.log("t");
+
+
+function Myname() {
+    console.log("s");
+console.log("a");
+console.log("n");
+console.log("k");
+console.log("e");
+console.log("t");
+
+}
+
+// Myname()
+
+function addNumber(number1,number2) {
+ console.log(number1+number2);
+}
+// addNumber(11,66)
+// console.log(addNumber(1,2)); //output undefined
+
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // let result = console.log(number1 + number2);
+    // return result
+    // return number1 + number2
+}
+// addTwoNumbers(3,5)
+// console.log(addTwoNumbers(3, 5));
+// const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "sk"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage())
+// console.log(loginUserMessage(""))
+// console.log(loginUserMessage("sanket"))
+
