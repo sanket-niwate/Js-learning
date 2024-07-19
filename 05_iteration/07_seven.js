0c2ac275772a971c87b211eab6084e26975bbc47
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-07-19 10:48:30 +0000

    filter map reduce

---
+++ 05_iteration/07_seven.js
@@ -0,0 +1,11 @@
+//The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array.
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
