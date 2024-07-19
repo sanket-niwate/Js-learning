de9668bd1d0fc80c4329be4ff67afb066c4f9bb7
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-07-19 10:55:16 +0000

    reduce

--- 05_iteration/08_eight.js
+++ 05_iteration/08_eight.js
@@ -1,3 +1,7 @@
+//The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.
+
+//The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place. Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
+
 const myNums = [1, 2, 3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
