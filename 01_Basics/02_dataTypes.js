ede8a3f0a42eac48f6eebece21e18a404154b2c5
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-12-14 14:42:53 +0530

    Update 02_dataTypes.js

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -1,6 +1,6 @@
-"use strict";//treat all js code as newer version
+"use strict";//treat all js code as a newer version
 
-//alert (3+3) // we are using nodejs nor browser
+//alert (3+3) // we are using nodejs not browser
 
 
 console.log(3+
@@ -14,18 +14,27 @@ console.log(3+
     let isLogged =false
 
 //primitive datatypes
-    // number => 2 to power 53
-    // bigint
-    // string=> ""
+    // number => 2 to power 53 //Represents numeric values, including integers and floating-point numbers.
+    // bigint => Represents numbers larger than 2^53 - 1, which is the largest number JavaScript can represent with the Number type.
+    // string=>'', "",``. //Represents a sequence of characters enclosed in single, double, or backticks.
     // boolean=> true/false
-    // null => standalone CSSMathValue   //null is an object
-    // undefined =>
-    // symvbol=>unique
+    // null => standalone Value   //null is an object //Represents an intentional absence of any object value.
+    // undefined => Represents a variable that has been declared but not assigned a value.
+    // symbol=>unique //Represents a unique identifier. Often used as object property keys.
 
-
+// Immutable: Primitive values cannot be changed; a new value is created if modified.
+// Type Check: Use typeof to check the type of a value.
+// Example:
+// console.log(typeof 42);         // "number"
+// console.log(typeof "Hello");    // "string"
+// console.log(typeof true);       // "boolean"
+// console.log(typeof undefined);  // "undefined"
+// console.log(typeof null);       // "object" (historical bug in JS)
+// console.log(typeof Symbol());   // "symbol"
+// console.log(typeof 123n);       // "bigint"
 
 
     // Object
 
     console.log(typeof null);//object
-    console.log(typeof undefined);//object
+    console.log(typeof undefined);//undefined
