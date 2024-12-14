06398f46b784f7267b0e8673a9cf17a3fdc84d75
Sanket Sanjay Niwate <137911756+sanket-niwate@users.noreply.github.com>
2024-12-14 14:11:13 +0530

    Update 01_variables.js

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,17 +1,22 @@
 const accoutNo =454544;
 let accountEmail ="sanekt@gmail.com";
 var accountPassword="446564"
-accountCity ="jaipur"
+accountCity ="Jaipur"
 let accountState; //output is undefined
 
 // accoutNo =5 //not allowed
 
 accountEmail="sanket1@gmail.com"
 accountPassword="675466"
-accountCity="mumbai"
+accountCity="Mumbai"
 console.log(accoutNo);
 
 /*prefer not to use var
-because of issue in block scope and functional scope*/
+because of issues in block scope and functional scope*/
 
 console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
+/* {
+This is the scope
+}*/
